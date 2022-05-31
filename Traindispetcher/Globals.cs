fecfe17af97f701eb0d8935323b906ac24c7ae1e
Олексій Рощупкін <MorenNeed@gmail.com>
2022-05-31 16:21:53 +0300

    Початок реалізації UC01 - додавання авторизації. Створено форму LoginForm.

--- Traindispetcher/Globals.cs
+++ Traindispetcher/Globals.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Traindispetcher
 {
-    public class Trainride
+    public class GlobalClass
     {
-        public int id { get; set; }
-        public string number { get; set; }
-        public string city { get; set; }
-        public System.TimeSpan departure_time { get; set; }
-        public int free_seats { get; set; }
-        public Trainride(int idNum, string nF, string cF, System.TimeSpan tF, int fS) 
+        public class Trainride
         {
-            this.id = idNum;
-            this.number = nF;
-            this.city = cF;
-            this.departure_time = tF;
-            this.free_seats = fS;
+            public int id { get; set; }
+            public string number { get; set; }
+            public string city { get; set; }
+            public System.TimeSpan departure_time { get; set; }
+            public int free_seats { get; set; }
+            public Trainride(int idNum, string nF, string cF, System.TimeSpan tF, int fS)
+            {
+                this.id = idNum;
+                this.number = nF;
+                this.city = cF;
+                this.departure_time = tF;
+                this.free_seats = fS;
+            }
         }
     }
 }
--- Traindispetcher/MainWindow.xaml.cs
+++ Traindispetcher/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿using System.Windows;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 namespace Traindispetcher
 {
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -23,6 +12,57 @@ namespace Traindispetcher
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void InfoTrainrideForm_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*
+            selTrainrideGroupBox.Visibility = Visibility.Hidden;
+
+            DataConnection = new DataAccess();
+
+            TrainrideListDG.ItemsSource = DataConnection.fList;
+
+            TrainrideGroupBox.Visibility = Visibility.Hidden;
+            
+            this.SizeToContent = SizeToContent.Manual;
+            TrainrideListDG.Height = 420;
+            this.Width = TrainrideListDG.Margin.Left + TrainrideListDG.RenderSize.Width + 90;
+            this.Height = TrainrideListDG.Margin.Top + TrainrideListDG.RenderSize.Height + 90;
+            */
+
+            TrainrideMenuItem.Visibility = Visibility.Hidden;
+            TrainrideMenuItem.Width = 0;
+        }
+
+        private void SaveDataMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            if ((selTrainrideGroupBox.Visibility == Visibility.Visible) && (selXY.selectedCityList.Count > 0))
+            {
+                selXY.WriteData(selXY.selectedCityList, selXY.selectedCityTimeList)
+            }
+
+            if (TrainrideGroupBox.Visibility == Visibility.Visible)
+            {
+                if ((TrainrideListDG.SelectedIndex < 0) && (!editedRow.flightAdd))
+                {
+                    MessageBox.Show("Оберіть у списку потяг для редагування подвійним кліком", "Увага!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+                else
+                {
+                    ChangeTrainrideListData(editedRow.trainrideNum);
+
+                    editedRow.ChangeDBRow();
+                }
+            }
+            */
+        }
+
+        private void AuthMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            LogInForm logWnd = new LogInForm();
+            logWnd.Show();
+            this.Visibility = Visibility.Collapsed;
         }
     }
 }
