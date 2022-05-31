ebab0b8b0c025bf9c0e90c9108228f01f64a433c
Олексій Рощупкін <MorenNeed@gmail.com>
2022-05-31 13:06:40 +0300

    Створено структуру даних з інформацією про потяги.

---
+++ Traindispetcher/Globals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traindispetcher
+{
+    public class Trainride
+    {
+        public int id { get; set; }
+        public string number { get; set; }
+        public string city { get; set; }
+        public System.TimeSpan departure_time { get; set; }
+        public int free_seats { get; set; }
+        public Trainride(int idNum, string nF, string cF, System.TimeSpan tF, int fS) 
+        {
+            this.id = idNum;
+            this.number = nF;
+            this.city = cF;
+            this.departure_time = tF;
+            this.free_seats = fS;
+        }
+    }
+}
